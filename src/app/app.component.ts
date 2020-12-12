90fba212e7c9279b11f317561459816be1a63fee
raghava352 <rakesh.veerabhotla@gmail.com>
2020-12-11 19:05:18 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component, VERSION } from "@angular/core";
 
 @Component({
   selector: "my-app",
+  //selector: ".my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
+  // template: `
+  //   <h1>{{ title }} works</h1>
+  //   <p>a long inline template</p>
+  // `,
+  //styles: ["h1 { font-weight: bold; }"]
 })
 export class AppComponent {
-  title = "app";
+  //title = "app";
+
+  title = "Angular Property Binding Example";
+
+  //Example 1
+  isDisabled = true;
+
+  firstName = "Sachin";
+  lastName = "Tendulkar";
+  nullItem = null;
+  data = null;
+
+  evilText = 'Template <script>alert("You are hacked")</script> Syntax';
+
+  script = '<script>alert("You are hacked")</script>';
+  div = "<div>this is a div</div>";
 }
