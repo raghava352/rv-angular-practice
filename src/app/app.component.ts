2fc59104830f3e6b63b3780133475750ac530c0a
raghava352 <rakesh.veerabhotla@gmail.com>
2020-12-14 16:22:51 -0500

    5 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,69 @@ import { FormsModule } from "@angular/forms";
 export class AppComponent {
   //title = "app";
 
-  title = "Angular Property Binding Example";
+  // title = "Angular Property Binding Example";
 
-  //Example 1
-  isDisabled = true;
+  // //Example 1
+  // isDisabled = true;
 
-  firstName = "Sachin";
-  lastName = "Tendulkar";
-  nullItem = null;
-  data = null;
+  // firstName = "Sachin";
+  // lastName = "Tendulkar";
+  // nullItem = null;
+  // data = null;
 
-  evilText = 'Template <script>alert("You are hacked")</script> Syntax';
+  // evilText = 'Template <script>alert("You are hacked")</script> Syntax';
 
-  script = '<script>alert("You are hacked")</script>';
-  div = "<div>this is a div</div>";
+  // script = '<script>alert("You are hacked")</script>';
+  // div = "<div>this is a div</div>";
 
-  name = "";
-  value = "";
-  clearValue() {
-    this.value = "";
+  // value = "";
+  // clearValue() {
+  //   this.value = "";
+  // }
+
+  title: string = "Top 5 Movies";
+
+  movies: Movie[] = [
+    {
+      title: "Zootopia",
+      director: "Byron Howard, Rich Moore",
+      cast: "Idris Elba, Ginnifer Goodwin, Jason Bateman",
+      releaseDate: "March 4, 2016"
+    },
+    {
+      title: "Batman v Superman: Dawn of Justice",
+      director: "Zack Snyder",
+      cast: "Ben Affleck, Henry Cavill, Amy Adams",
+      releaseDate: "March 25, 2016"
+    },
+    {
+      title: "Captain American: Civil War",
+      director: "Anthony Russo, Joe Russo",
+      cast: "Scarlett Johansson, Elizabeth Olsen, Chris Evans",
+      releaseDate: "May 6, 2016"
+    },
+    {
+      title: "X-Men: Apocalypse",
+      director: "Bryan Singer",
+      cast: "Jennifer Lawrence, Olivia Munn, Oscar Isaac",
+      releaseDate: "May 27, 2016"
+    },
+    {
+      title: "Warcraft",
+      director: "Duncan Jones",
+      cast: "Travis Fimmel, Robert Kazinsky, Ben Foster",
+      releaseDate: "June 10, 2016"
+    }
+  ];
+
+  CompositeKey(index, item) {
+    return item.title + item.director;
   }
+}
+
+class Movie {
+  title: string;
+  director: string;
+  cast: string;
+  releaseDate: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { CustomerListComponent } from "./customer-list.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, CustomerListComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
